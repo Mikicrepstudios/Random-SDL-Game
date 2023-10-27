d6f1c774f557bd9dca48445d1c57d3b6e3014010
Mikicrep <mikicrepoffical@gmail.com>
2023-10-27 17:14:48 +0200

    Build 10 | Fix out of bounds bug

--- player.h
+++ player.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace player {
-    extern void playermovement(SDL_Event event, int worldmap[250][250], int playerspeed, int &playerx, int &playery);
+    extern void playermovement(SDL_Event event, int worldmap[250][250], int mapwidth, int mapheight, int playerspeed, int &playerx, int &playery);
     extern void mouseevent(SDL_Event event, int worldmap[250][250], int mapwidth, int mapheight, int curhoverx, int curhovery, int &curblock, int camoffsetx, int camoffsety);
     void inventoryevent(SDL_Event event, bool &inventory, int &curblock);
 }
--- playermovement.cpp
+++ playermovement.cpp
@@ -1,26 +1,26 @@
 #include "SDL2/SDL.h"
 
 namespace player {
-    void playermovement(SDL_Event event, int worldmap[250][250], int playerspeed, int &playerx, int &playery) {
+    void playermovement(SDL_Event event, int worldmap[250][250], int mapwidth, int mapheight, int playerspeed, int &playerx, int &playery) {
         if(event.key.keysym.sym == SDLK_w) {
-            if (worldmap[playerx][playery - playerspeed] == 0) {
+            if (worldmap[playerx][playery - playerspeed] == 0 && playery != 0) {
                 worldmap[playerx][playery] = 0;
                 playery -= playerspeed;
             }
         }
         else if(event.key.keysym.sym == SDLK_a) {
-            if (worldmap[playerx - playerspeed][playery] == 0) {
+            if (worldmap[playerx - playerspeed][playery] == 0 && playerx != 0) {
                 worldmap[playerx][playery] = 0;
                 playerx -= playerspeed;
             }
         }
-        else if(event.key.keysym.sym == SDLK_s) {
+        else if(event.key.keysym.sym == SDLK_s && playery != mapheight) {
             if (worldmap[playerx][playery + playerspeed] == 0) {
                 worldmap[playerx][playery] = 0;
                 playery += playerspeed;
             }
         }
-        else if(event.key.keysym.sym == SDLK_d) {
+        else if(event.key.keysym.sym == SDLK_d && playerx != mapwidth) {
             if (worldmap[playerx + playerspeed][playery] == 0) {
                 worldmap[playerx][playery] = 0;
                 playerx += playerspeed;
