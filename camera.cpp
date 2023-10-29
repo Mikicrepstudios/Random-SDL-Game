ea6ca1b5d65deb28d429ddbeaac6af6714400079
Mikicrep <mikicrepoffical@gmail.com>
2023-10-29 11:22:02 +0100

    Build 11 | Fix cam x -1 y -1

--- camera.cpp
+++ camera.cpp
@@ -4,10 +4,14 @@ namespace events {
     void camera(SDL_Event event, bool inventory, int &camoffsetx, int &camoffsety, int &camscale) {
         if (!inventory) {
             if(event.key.keysym.sym == SDLK_UP) {
-                camoffsety += 1;
+                if(camoffsety != 0) {
+                    camoffsety += 1;
+                }
             }
             else if(event.key.keysym.sym == SDLK_LEFT) {
-                camoffsetx += 1;
+                if(camoffsetx != 0) {
+                    camoffsetx += 1;
+                }
             }
             else if(event.key.keysym.sym == SDLK_DOWN) {
                 camoffsety -= 1;
