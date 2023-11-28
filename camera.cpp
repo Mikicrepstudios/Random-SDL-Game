2989f82cc03aaac0bcab04062501e0e3992f8a69
Mikicrep <mikicrepoffical@gmail.com>
2023-11-29 00:56:31 +0100

    Build 15 | 61 lines less (Shorter vars, less nested ifs), Fix bug button clicking outside inv

--- camera.cpp
+++ camera.cpp
@@ -3,15 +3,11 @@
 namespace events {
     void camera(SDL_Event event, bool inventory, int &camoffsetx, int &camoffsety, int &camscale) {
         if (!inventory) {
-            if(event.key.keysym.sym == SDLK_UP) {
-                if(camoffsety != 0) {
-                    camoffsety += 1;
-                }
+            if(event.key.keysym.sym == SDLK_UP && camoffsety != 0) {
+                camoffsety += 1;
             }
-            else if(event.key.keysym.sym == SDLK_LEFT) {
-                if(camoffsetx != 0) {
-                    camoffsetx += 1;
-                }
+            else if(event.key.keysym.sym == SDLK_LEFT && camoffsetx != 0) {
+                camoffsetx += 1;
             }
             else if(event.key.keysym.sym == SDLK_DOWN) {
                 camoffsety -= 1;
@@ -20,10 +16,8 @@ namespace events {
                 camoffsetx -= 1;
             }
         }
-        if(event.key.keysym.sym == SDLK_o) {
-            if (camscale != 3) {
-                camscale -= 1;
-            }
+        if(event.key.keysym.sym == SDLK_o && camscale != 3) {
+            camscale -= 1;
         }
         else if(event.key.keysym.sym == SDLK_p) {
             camscale += 1;
--- playermovement.cpp
+++ playermovement.cpp
@@ -2,29 +2,21 @@
 
 namespace player {
     void playermovement(SDL_Event event, int worldmap[250][250], int mapwidth, int mapheight, int playerspeed, int &playerx, int &playery) {
-        if(event.key.keysym.sym == SDLK_w) {
-            if (worldmap[playerx][playery - playerspeed] == 0 && playery != 0) {
-                worldmap[playerx][playery] = 0;
-                playery -= playerspeed;
-            }
+        if(event.key.keysym.sym == SDLK_w && worldmap[playerx][playery - playerspeed] == 0 && playery != 0) {
+            worldmap[playerx][playery] = 0;
+            playery -= playerspeed;
         }
-        else if(event.key.keysym.sym == SDLK_a) {
-            if (worldmap[playerx - playerspeed][playery] == 0 && playerx != 0) {
-                worldmap[playerx][playery] = 0;
-                playerx -= playerspeed;
-            }
+        else if(event.key.keysym.sym == SDLK_a && worldmap[playerx - playerspeed][playery] == 0 && playerx != 0) {
+            worldmap[playerx][playery] = 0;
+            playerx -= playerspeed;
         }
-        else if(event.key.keysym.sym == SDLK_s && playery != mapheight) {
-            if (worldmap[playerx][playery + playerspeed] == 0) {
-                worldmap[playerx][playery] = 0;
-                playery += playerspeed;
-            }
+        else if(event.key.keysym.sym == SDLK_s && playery != mapheight && worldmap[playerx][playery + playerspeed] == 0) {
+            worldmap[playerx][playery] = 0;
+            playery += playerspeed;
         }
-        else if(event.key.keysym.sym == SDLK_d && playerx != mapwidth) {
-            if (worldmap[playerx + playerspeed][playery] == 0) {
-                worldmap[playerx][playery] = 0;
-                playerx += playerspeed;
-            }
+        else if(event.key.keysym.sym == SDLK_d && playerx != mapwidth && worldmap[playerx + playerspeed][playery] == 0) {
+            worldmap[playerx][playery] = 0;
+            playerx += playerspeed;
         }
     }
 }
