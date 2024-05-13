75ba5e0e216dc357b5aa82a5dcaecbc0c6eb2aac
Mikicrep <mikicrepoffical@gmail.com>
2024-05-13 20:57:17 +0200

    Cleaning up old files (2/2)

--- files.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "block.h"
-
-namespace files {
-    extern void SaveMap(SDL_Event event, Block worldMap[250][250], int mapWidth, int mapHeight);
-    extern void LoadMap(SDL_Event event, Block worldMap[250][250], int mapWidth, int mapHeight);
-}
--- graphics.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace graphics {
-    extern void GetColor(int colorId, int &colorR, int &colorG, int &colorB);
-}
