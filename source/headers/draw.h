7daf0fca0058969c0a0b774b7ec0fbee7b7c66a2
Mikicrep <mikicrepoffical@gmail.com>
2024-11-12 23:14:34 +0100

    Migrated main.cpp to MF

--- source/headers/draw.h
+++ source/headers/draw.h
@@ -4,9 +4,4 @@
 
 namespace draw {
 	extern void SetCol(SDL_Renderer* renderer, int bgColor);
-
-	extern void DrawButton(SDL_Renderer* renderer, SDL_Rect rect, int colorid, int mouseX, int mouseY);
-	extern void DrawRect(SDL_Renderer* renderer, SDL_Rect rect, int colorid);
-	extern void DrawTextureRect(SDL_Renderer* renderer, SDL_Rect rect, SDL_Texture* texture);
-	extern void DrawText(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect rect, const char* text, SDL_Color color);
 }
