ed9e088922e605e30d4395ef654d86b5351cc650
Mikicrep <mikicrepoffical@gmail.com>
2024-11-13 00:20:23 +0100

    Migrated cheats.cpp to MF

--- source/player/cheats.cpp
+++ source/player/cheats.cpp
@@ -1,24 +1,26 @@
 #include "SDL2/SDL.h"
 
+#include "mf/core.h"
+
 #include "block.h"
 #include "settings.h"
 
 namespace cheats {
-	int CamTp(game::SDL_Settings sdlSettings, game::Settings &settings, game::Camera &cam) {
-		if (sdlSettings.event.type == SDL_MOUSEBUTTONDOWN) {
-			cam.offSetX -= sdlSettings.curHoverX;
-			cam.offSetY -= sdlSettings.curHoverY;
+	int CamTp(core::MF_Window &window, game::Game game, game::Settings &settings, game::Camera &cam) {
+		if (window.event.type == SDL_MOUSEBUTTONDOWN) {
+			cam.offSetX -= game.curHoverX;
+			cam.offSetY -= game.curHoverY;
 
 			return 1;
 		}
 
 		return 0;
 	}
-	int PlayerTp(game::SDL_Settings sdlSettings, game::Settings &settings, game::Map &map, game::Camera &cam, game::Player &player) {
-		if (sdlSettings.event.type == SDL_MOUSEBUTTONDOWN) {
+	int PlayerTp(core::MF_Window &window, game::Game game, game::Settings &settings, game::Map &map, game::Camera &cam, game::Player &player) {
+		if (window.event.type == SDL_MOUSEBUTTONDOWN) {
 			map.map[player.x][player.y].type = 0; // Remove current player so you dont have ghost player
-			player.x = -cam.offSetX + sdlSettings.curHoverX;
-			player.y = -cam.offSetY + sdlSettings.curHoverY;
+			player.x = -cam.offSetX + game.curHoverX;
+			player.y = -cam.offSetY + game.curHoverY;
 
 			return 1;
 		}
