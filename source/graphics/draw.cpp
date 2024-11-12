fe4fb085efeba9f6f4d7b5c60018cb08f7509949
Mikicrep <mikicrepoffical@gmail.com>
2024-11-12 23:49:17 +0100

    Migrated draw.cpp to MF

--- source/graphics/draw.cpp
+++ source/graphics/draw.cpp
@@ -2,50 +2,10 @@
 #include "SDL2/SDL.h"
 #include <SDL_ttf.h>
 
-#include "graphics.h"
-
-graphics::Colors* color = graphics::InitColors();
+#include "mf/colors.h"
 
 namespace draw {
 	void SetCol(SDL_Renderer* renderer, int bgColor) {
-		SDL_SetRenderDrawColor(renderer, color[bgColor - 1].r, color[bgColor - 1].g, color[bgColor - 1].b, 255);
-	}
-
-	void DrawButton(SDL_Renderer* renderer, SDL_Rect rect, int colorid, int mouseX, int mouseY) {
-		int colorR = color[colorid - 1].r;
-		int colorG = color[colorid - 1].g;
-		int colorB = color[colorid - 1].b;
-		if (mouseX >= rect.x && mouseX <= rect.x + rect.w &&
-			mouseY >= rect.y && mouseY <= rect.y + rect.h) {
-			colorR *= .25;
-			colorG *= .25;
-			colorB *= .25;
-			SDL_SetRenderDrawColor(renderer, colorR, colorG, colorB, 255);
-			SDL_RenderFillRect(renderer, &rect);
-		}
-		else
-			SDL_SetRenderDrawColor(renderer, colorR, colorG, colorB, 255);
-		SDL_RenderFillRect(renderer, &rect);
-	}
-
-	void DrawRect(SDL_Renderer* renderer, SDL_Rect rect, int colorid) {
-		if(colorid - 1 >= 0 && colorid <= 32) {
-			SDL_SetRenderDrawColor(renderer, color[colorid - 1].r, color[colorid - 1].g, color[colorid - 1].b, 255);
-			SDL_RenderFillRect(renderer, &rect);
-		}
-		//else if(colorid - 1 < 0) std::cout << "DrawRect() got colorid less than 0" << std::endl;
-		//else if(colorid - 1 > 32) std::cout << "DrawRect() got color id bigger than 32" << std::endl;
-	}
-
-	void DrawTextureRect(SDL_Renderer* renderer, SDL_Rect rect, SDL_Texture* texture) {
-		SDL_RenderCopy(renderer, texture, NULL, &rect);
-	}
-
-	void DrawText(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect rect, const char* text, SDL_Color textColor) {
-		SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, textColor);
-		SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-		SDL_RenderCopy(renderer, textTexture, NULL, &rect);
-		SDL_FreeSurface(textSurface);
-        SDL_DestroyTexture(textTexture);
+		SDL_SetRenderDrawColor(renderer, colors::colorID[bgColor - 1].r, colors::colorID[bgColor - 1].g, colors::colorID[bgColor - 1].b, 255);
 	}
 }
