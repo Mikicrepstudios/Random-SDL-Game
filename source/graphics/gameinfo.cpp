b6272dddd933fffabfa11e8fe52cab5e1977f1ee
Mikicrep <mikicrepoffical@gmail.com>
2024-11-12 23:53:00 +0100

    Migrated gameinfo.cpp to MF

--- source/graphics/gameinfo.cpp
+++ source/graphics/gameinfo.cpp
@@ -1,23 +1,24 @@
-#include "SDL2/SDL.h"
-#include <SDL_ttf.h>
 #include <string>
 
-#include "draw.h"
+#include "mf/core.h"
+#include "mf/colors.h"
+#include "mf/graphics.h"
+
 #include "settings.h"
 
 namespace hud {
-	void GameInfo(game::SDL_Settings sdlSettings, game::Settings settings, game::Camera cam, game::Player player) {
-		SDL_Color color = {};
+	void GameInfo(core::MF_Window &window, game::Settings settings, game::Camera cam, game::Player player) {
+		MF_Color color = {};
 		// If background is white then set text color to black otherwise its white
 		if(settings.bgColor == 32)
-			color = sdlSettings.altTextColor;
+			color = colors::black;
 		else
-			color = sdlSettings.textColor;
+			color = colors::white;
 		
 		std::string gameInfoTexts[16] = {
-			"Width: " + std::to_string(sdlSettings.width),
-			"Height: " + std::to_string(sdlSettings.height),
-			"Fps: " + std::to_string(sdlSettings.fps),
+			"Width: " + std::to_string(window.width),
+			"Height: " + std::to_string(window.height),
+			"Fps: " + std::to_string(window.fps),
 			"", // Empty string for skipped line
 			"Cur preset: " + std::to_string(settings.curPreset + 1),
 			"Block color: " + std::to_string(settings.blockColor),
@@ -34,7 +35,7 @@ namespace hud {
 		// Draw gameInfo hud
 		for(int i = 0; i <= 13; i++) {
 			SDL_Rect curRect = {0, i * 20, 100, 20};
-			draw::DrawText(sdlSettings.renderer, sdlSettings.font, curRect, gameInfoTexts[i].c_str(), color);
+			draw::DrawText(window.renderer, window.font, curRect, gameInfoTexts[i].c_str(), color);
 		}
 	}
 }
