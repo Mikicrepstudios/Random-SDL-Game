cdc2be42ceffa22f58ebdfd74a746f4ee6e127fd
Mikicrep <mikicrepoffical@gmail.com>
2024-11-13 00:26:53 +0100

    Migrated playercontrols.cpp to MF

--- source/player/playercontrols.cpp
+++ source/player/playercontrols.cpp
@@ -1,55 +1,58 @@
 #include "SDL2/SDL.h"
-#include <iostream>
-#include "draw.h"
+
+#include "mf/core.h"
+#include "mf/colors.h"
+#include "mf/graphics.h"
+
 #include "block.h"
 #include "presets.h"
 #include "settings.h"
 
 namespace mouse {
-	void Event(SDL_Event event, game::SDL_Settings sdlSettings, game::Settings &settings, game::Map &map, game::Camera &cam, game::Preset preset[10]) {
-		if (sdlSettings.curHoverX <= map.width && sdlSettings.curHoverY <= map.height && !settings.inventory && map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].type != 1) {
+	void Event(core::MF_Window &window, game::Game game, game::Settings &settings, game::Map &map, game::Camera &cam, game::Preset preset[10]) {
+		if (game.curHoverX <= map.width && game.curHoverY <= map.height && !settings.inventory && map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].type != 1) {
 			if (!settings.cheats) {
-				if(sdlSettings.isMouseDown) {
+				if(window.isMouseDown) {
 					Uint32 mouseButtons = SDL_GetMouseState(NULL, NULL);
 					if (mouseButtons & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].type = 2;
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].color = settings.blockColor;
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].texture = settings.blockTextureId;
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].isSolid = settings.placeSolidBlocks;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].type = 2;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].color = settings.blockColor;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].texture = settings.blockTextureId;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].isSolid = settings.placeSolidBlocks;
 					}
 					else if (mouseButtons & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].type = 0;
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].color = 0;
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].texture = 0;
-						map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].isSolid = false;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].type = 0;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].color = 0;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].texture = 0;
+						map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].isSolid = false;
 					}
 				}
 			}
-			else if (event.type == SDL_MOUSEBUTTONDOWN && settings.cheats && settings.cheatsId == 3) {
-				if (map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].type == 2) {
-					preset[settings.curPreset].blockColor = map.map[sdlSettings.curHoverX - cam.offSetX][sdlSettings.curHoverY - cam.offSetY].color;
+			else if (window.event.type == SDL_MOUSEBUTTONDOWN && settings.cheats && settings.cheatsId == 3) {
+				if (map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].type == 2) {
+					preset[settings.curPreset].blockColor = map.map[game.curHoverX - cam.offSetX][game.curHoverY - cam.offSetY].color;
 					settings.cheats = false;
 				}
 			}
 		}
 	}
-	void Overlay(game::SDL_Settings sdlSettings, game::Settings settings, game::Map map, game::Camera cam) {
-		int curHoverX = sdlSettings.curHoverX;
-		int curHoverY = sdlSettings.curHoverY;
+	void Overlay(core::MF_Window &window, game::Game game, game::Settings settings, game::Map map, game::Camera cam) {
+		int curHoverX = game.curHoverX;
+		int curHoverY = game.curHoverY;
 
 		if (curHoverX <= map.width && curHoverY <= map.height && map.map[curHoverX - cam.offSetX][curHoverY - cam.offSetY].type != 1 && !settings.inventory) {
 			SDL_Rect mouseRect = {curHoverX * cam.scale, curHoverY * cam.scale, cam.scale, cam.scale};
 			if (cam.highlight == false) {
                 if (settings.bgColor == 32 || map.map[curHoverX - cam.offSetX][curHoverY - cam.offSetY].color == 32)
-					draw::DrawRect(sdlSettings.renderer, mouseRect, 1);
+					draw::DrawRect(window.renderer, mouseRect, colors::black);
 				else
-					draw::DrawRect(sdlSettings.renderer, mouseRect, 32);
+					draw::DrawRect(window.renderer, mouseRect, colors::white);
 			}
 			else {
 				if (settings.bgColor == 27)
-					draw::DrawRect(sdlSettings.renderer, mouseRect, 26);
+					draw::DrawRect(window.renderer, mouseRect, colors::darkred);
 				else
-					draw::DrawRect(sdlSettings.renderer, mouseRect, 27);
+					draw::DrawRect(window.renderer, mouseRect, colors::red);
 			}
 		}
 	}
