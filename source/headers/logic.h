66d6c613e76f314cd8bc97b9ec7265674bfe1e35
Mikicrep <mikicrepoffical@gmail.com>
2024-11-12 23:56:07 +0100

    Migrate updatevars.cpp to MF

--- source/headers/logic.h
+++ source/headers/logic.h
@@ -4,5 +4,5 @@
 #include "settings.h"
 
 namespace logic {
-    extern void UpdateVars(game::Settings &settings, game::Player &player, game::Camera &cam, game::Preset preset[10]);
+    void UpdateVars(game::Settings &settings, game::Player &player, game::Camera &cam, game::Preset preset[10]);
 }
