c61f39e4f6aa933a9db308d87a858e4aec585fe2
Mikicrep <mikicrepoffical@gmail.com>
2024-11-29 22:42:27 +0100

    [WINFIX 2] Tried disabling chooser entirely

--- source/main.cpp
+++ source/main.cpp
@@ -226,9 +226,9 @@ int main(int argc, char **argv) {
 
 			if(settings.canPlayerPlace == true) mouse::Event(window, game, settings, map, cam, preset);
 
-			std::cout << "Running inventory function" << std::endl;
+			/*std::cout << "Running inventory function" << std::endl;
 			if(settings.inventory) inventory::Chooser(window, settings, map, player, cam, preset, inventoryMenuRects, inventoryColorRects, inventoryDecalRects, inventoryGameplayRects, inventoryGameRects, inventoryOtherRects);
-			std::cout << "Finished running inventory function :D" << std::endl;
+			std::cout << "Finished running inventory function :D" << std::endl;*/
 			}
 
 		// Set BG color to new color
