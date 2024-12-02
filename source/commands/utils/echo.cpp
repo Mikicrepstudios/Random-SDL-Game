e563f1ab8c2e275f37087d46d20ee13d31af157b
Mikicrep <mikicrepoffical@gmail.com>
2024-12-02 12:16:44 +0100

    Document echo.cpp

--- source/commands/utils/echo.cpp
+++ source/commands/utils/echo.cpp
@@ -3,6 +3,10 @@
 
 namespace commands {
     int Echo(std::string args) {
+        /**
+         * @brief Function for Echo command
+         * @param args Text to print
+         */
         std::cout << args << std::endl;
 
         return 0;
