cb563aedfc2ff3207d278de5207da2bc18836c32
Mikicrep <mikicrepoffical@gmail.com>
2024-12-02 12:17:41 +0100

    Document getbgcolor.cpp

--- source/commands/utils/getbgcolor.cpp
+++ source/commands/utils/getbgcolor.cpp
@@ -2,6 +2,10 @@
 
 namespace commands {
     int GetBGColor(int bgColor) {
+        /**
+         * @brief Function for GetBGColor command
+         * @param bgColor BG Color
+         */
         std::cout << bgColor << std::endl;
 
         return 0;
