9a4ce36eceacf52954ef86acbbe303de31cbd0a5
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:56:34 +0100

    Document map.cpp

--- source/map/map.cpp
+++ source/map/map.cpp
@@ -2,6 +2,11 @@
 
 namespace gamemap {
 	void ClearMap(game::Map &map) {
+		/**
+		 * @brief This function clears map
+		 * @param map Game map which will be cleared
+		 */
+
 		for (int x = 0; x < map.width; x++) {
 			for (int y = 0; y < map.height; y++) {
 				map.map[x][y].type = 0;
