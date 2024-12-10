e17b9ac4f822ff1b8344abd84aaaa40b83e22427
Mikicrep <mikicrepoffical@gmail.com>
2024-12-11 00:38:54 +0100

    Remove logic.h

--- source/headers/game.h
+++ source/headers/game.h
@@ -9,6 +9,9 @@
  * @brief Namespace that contains all game core functions
  */
 namespace game {
+	// Core
+    void UpdateVars(game::Settings &settings, game::Player &player, game::Camera &cam, game::Preset preset[10]);
+	
 	// Essential stuff
 	void CameraControl(core::MF_Window &window, game::Settings settings, game::Camera &cam);
 
