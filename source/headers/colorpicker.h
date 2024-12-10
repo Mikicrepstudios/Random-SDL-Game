15f529b8cc34ecab0c8bb65163e23d7f0cf54497
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:45:25 +0100

    Document colorpicker.cpp

--- source/headers/colorpicker.h
+++ source/headers/colorpicker.h
@@ -4,6 +4,9 @@
 
 #include "settings.h"
 
+/**
+ * @brief Namespace that contains all color picker related stuff
+ */
 namespace colorpicker {
     void Overlay(core::MF_Window &window, game::Settings settings);
 	int Event(core::MF_Window &window, game::Settings &settings);
