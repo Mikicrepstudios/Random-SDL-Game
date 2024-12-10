e28049a67dc6eb1a58517a5a6ce4e0e18bdcbea8
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:44:45 +0100

    Document cheats.h

--- source/headers/cheats.h
+++ source/headers/cheats.h
@@ -4,6 +4,9 @@
 
 #include "settings.h"
 
+/**
+ * @brief Namespace that contains all game cheats
+ */
 namespace cheats {
 	int CamTp(core::MF_Window &window, game::Game game, game::Settings &settings, game::Camera &cam);
 	int PlayerTp(core::MF_Window &window, game::Game game, game::Settings &settings, game::Map &map, game::Camera &camera, game::Player &cam);
