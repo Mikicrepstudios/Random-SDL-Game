f80dce243aac833de9ad5f0d0e1cdb4047459c5c
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:53:31 +0100

    DoCUMent player.h

--- source/headers/player.h
+++ source/headers/player.h
@@ -3,6 +3,9 @@
 #include "presets.h"
 #include "settings.h"
 
+/**
+ * @brief Namespace that contains all player related stuff
+ */
 namespace player {
 	void Movement(SDL_Event event, game::Map &map, game::Player &player);
 }
