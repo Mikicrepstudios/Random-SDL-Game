928e734cb1a9112282007b6efd6c1a68d0fec035
Mikicrep <mikicrepoffical@gmail.com>
2024-12-11 00:43:38 +0100

    Remove colorpicker.h

--- source/headers/gui.h
+++ source/headers/gui.h
@@ -9,5 +9,8 @@
  * @brief Namespace that contains all HUD related stuff
  */
 namespace gui {
+    void ColorPickerOverlay(core::MF_Window &window, game::Settings settings);
+	int ColorPickerEvent(core::MF_Window &window, game::Settings &settings);
+
     void GameInfo(core::MF_Window &window, game::Settings settings, game::Camera cam, game::Player player);
 }
