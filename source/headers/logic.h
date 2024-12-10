0e424392d2ab1052ba8130f11b84bffeb1086468
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:52:30 +0100

    Document logic.h

--- source/headers/logic.h
+++ source/headers/logic.h
@@ -3,6 +3,9 @@
 #include "presets.h"
 #include "settings.h"
 
+/**
+ * @brief Namespace that contains all game logic related stuff
+ */
 namespace logic {
     void UpdateVars(game::Settings &settings, game::Player &player, game::Camera &cam, game::Preset preset[10]);
 }
