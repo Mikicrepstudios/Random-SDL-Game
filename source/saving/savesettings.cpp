2c34d3087c12b645df114f231c1dfa20f6c7b58f
Mikicrep <mikicrepoffical@gmail.com>
2024-12-11 00:06:58 +0100

    Document savesettings.cpp

--- source/saving/savesettings.cpp
+++ source/saving/savesettings.cpp
@@ -6,6 +6,13 @@
 
 namespace files {
 	void SaveSettings(game::Settings settings, game::Player player, game::Camera cam) {
+		/**
+		 * @brief This function saves game settings to file
+		 * @param settings Game settings
+		 * @param player Game player
+		 * @param cam Game camera
+		 */
+		
 		std::ofstream settingsFile("save/settings.msave"); // Open file
 
 		// Save settings
@@ -26,6 +33,14 @@ namespace files {
 	}
 
 	void LoadSettings(game::Settings &settings, game::Player &player, game::Camera &cam, game::Preset preset[10]) {
+		/**
+		 * @brief This function loads saved game settings
+		 * @param settings Game settings
+		 * @param player Game player
+		 * @param cam Game camera
+		 * @param preset Game presets
+		 */
+		
 		std::ifstream settingsFile("save/settings.msave"); // Open file
 
 		// Load settings
