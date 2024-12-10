879f2d21d7c7904cebc752e6cebcc08bb925e1d9
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:57:56 +0100

    Document event.cpp

--- source/player/inventory/event.cpp
+++ source/player/inventory/event.cpp
@@ -2,6 +2,12 @@
 
 namespace inventory {
     void Event(SDL_Event event, game::Settings &settings) {
+		/**
+		 * @brief This function opens and closes inventory and its submenus
+		 * @param event SDL_Event
+		 * @param settings Game settings
+		 */
+		
 		if(event.key.keysym.sym == SDLK_e) {
 			if (!settings.inventory) {
 				settings.inventory = true;
