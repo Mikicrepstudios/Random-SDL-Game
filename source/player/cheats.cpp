f145ddbdc8db744134f841b99cf238b3eed64f6c
Mikicrep <mikicrepoffical@gmail.com>
2024-12-11 00:02:33 +0100

    Document cheats.cpp

--- source/player/cheats.cpp
+++ source/player/cheats.cpp
@@ -7,6 +7,14 @@
 
 namespace cheats {
 	int CamTp(core::MF_Window &window, game::Game game, game::Settings &settings, game::Camera &cam) {
+		/**
+		 * @brief This function executes Camera Teleport cheat
+		 * @param window Game window
+		 * @param game Game core
+		 * @param settings Game settings
+		 * @param cam Game camera
+		 */
+
 		if (window.event.type == SDL_MOUSEBUTTONDOWN) {
 			cam.offSetX -= game.curHoverX;
 			cam.offSetY -= game.curHoverY;
@@ -17,6 +25,16 @@ namespace cheats {
 		return 0;
 	}
 	int PlayerTp(core::MF_Window &window, game::Game game, game::Settings &settings, game::Map &map, game::Camera &cam, game::Player &player) {
+		/**
+		 * @brief This function executes Player Teleport cheat
+		 * @param window Game window
+		 * @param game Game core
+		 * @param settings Game settings
+		 * @param map Game map
+		 * @param cam Game camera
+		 * @param player Game player
+		 */
+		
 		if (window.event.type == SDL_MOUSEBUTTONDOWN) {
 			map.map[player.x][player.y].type = 0; // Remove current player so you dont have ghost player
 			player.x = -cam.offSetX + game.curHoverX;
