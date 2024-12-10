71a2212c3ab612bd845ee6c331dabcfdc8222bc2
Mikicrep <mikicrepoffical@gmail.com>
2024-12-10 23:51:20 +0100

    Document game.h

--- source/headers/game.h
+++ source/headers/game.h
@@ -5,14 +5,23 @@
 #include "settings.h"
 #include "textures.h"
 
+/**
+ * @brief Namespace that contains all game events
+ */
 namespace events {
 	void Camera(core::MF_Window &window, game::Settings settings, game::Camera &cam);
 }
 
+/**
+ * @brief Namespace that contains all game core functions
+ */
 namespace game {
 	void RenderMap(core::MF_Window &window, game::Settings settings, game::Map map, game::Camera cam, textures::BlockTextures blockTextures[32]);
 }
 
+/**
+ * @brief Namespace that contains all game map realted stuff
+ */
 namespace gamemap {
 	void ClearMap(game::Map &map);
 }
