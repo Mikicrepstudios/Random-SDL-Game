f238e89f21033b0a31deb010fca1d2505a878f63
Mikicrep <mikicrepoffical@gmail.com>
2024-12-21 22:12:42 +0100

    Update to Mikicrep Framework 1.5.0

---
+++ miki-framework/core/printver.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+namespace core {
+    void printver(std::string ver, int type) {
+        /**
+         * @brief You MUST use this function when you use Mikicrep Framework, it gives us credits for framwork and prints version for easier support
+         * @param ver MF Version
+         * @param type Type of version... Check below comments
+         */
+        /*
+        Type types
+        1 - Standard release
+        2 - Modified release - Use this if you modify MF code to your needs
+        3 - DEV version - This is used to tell that you are using version from beta channel
+        */
+        std::cout << "-------Mikicrep Framework-------" << std::endl;
+
+        if(type == 1)
+            std::cout << "-------Ver: " << ver << "---------------" << std::endl;
+        else if(type == 2)
+            std::cout << "-------Ver: " << ver << "M--------------" << std::endl;
+        else if(type == 3)
+              std::cout << "-------Ver: D E V---------------" << std::endl;
+
+        std::cout << "Copyright Mikicrep Studios 2024" << std::endl;
+    }
+}
---
+++ miki-framework/extra/casttoint.cpp
@@ -0,0 +1,9 @@
+namespace extra {
+    int castToInt(float value) {
+        /**
+         * @brief This function converts int number to float
+         * @param value Number to convert
+         */
+        return static_cast<int>(value);
+    }
+}
