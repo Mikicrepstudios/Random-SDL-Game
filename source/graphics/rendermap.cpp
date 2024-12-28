5d8cb84d2113e997d3f30512626cdf4d44ac1b95
Mikicrep <mikicrepoffical@gmail.com>
2024-12-28 14:18:38 +0100

    Added extra tabs in rendermap.cpp

--- source/graphics/rendermap.cpp
+++ source/graphics/rendermap.cpp
@@ -25,21 +25,21 @@ namespace game {
 		for(int x = 0; x <= maxXBlocks; x++) {
 			for(int y = 0; y <= maxYBlocks; y++) {
 				if((cam.offSetX - window.width / cam.scale) > -map.width) {
-				// Get current block id and define variables
-				int objectId = map.map[x - cam.offSetX][y - cam.offSetY].type;
-				int colorId = map.map[x - cam.offSetX][y - cam.offSetY].color;
-				int textureId = map.map[x - cam.offSetX][y - cam.offSetY].texture;
+					// Get current block id and define variables
+					int objectId = map.map[x - cam.offSetX][y - cam.offSetY].type;
+					int colorId = map.map[x - cam.offSetX][y - cam.offSetY].color;
+					int textureId = map.map[x - cam.offSetX][y - cam.offSetY].texture;
 
-				// Get color data
-				if(objectId != 0) {
-					SDL_Rect curRect = {x * cam.scale, y * cam.scale, cam.scale, cam.scale};
-					draw::DrawRect(window.renderer, curRect, colors::colorID[colorId - 1]);
+					// Get color data
+					if(objectId != 0) {
+						SDL_Rect curRect = {x * cam.scale, y * cam.scale, cam.scale, cam.scale};
+						draw::DrawRect(window.renderer, curRect, colors::colorID[colorId - 1]);
 
-					if(textureId != 0)
-						SDL_RenderCopy(window.renderer, blockTextures[textureId].texture, NULL, &curRect);
+						if(textureId != 0)
+							SDL_RenderCopy(window.renderer, blockTextures[textureId].texture, NULL, &curRect);
 
-					if(objectId == 1) draw::DrawRect(window.renderer, curRect, colors::colorID[settings.playerColor - 1]);
-				}
+						if(objectId == 1) draw::DrawRect(window.renderer, curRect, colors::colorID[settings.playerColor - 1]);
+					}
 				}
 			}
 		}
