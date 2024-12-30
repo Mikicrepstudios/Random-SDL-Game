1718999a592d2790248e7e76c19003b3a9b149df
Mikicrep <mikicrepoffical@gmail.com>
2024-12-30 20:31:49 +0100

    Update docs

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cameracontrols_0',['CameraControls',['../d3/d2c/namespacegame.html#a628e1f2bf2f88daf12986d4ac1b3f48e',1,'game']]],
+  ['cameracontrols_0',['CameraControls',['../d3/d2c/namespacegame.html#a8dd7690595775b1cf056d067568321ec',1,'game']]],
   ['camtp_1',['CamTp',['../df/dbb/namespacecheats.html#a076e67bf3c7971a9d8bd1a4e368c5107',1,'cheats']]],
   ['chooser_2',['Chooser',['../d5/d2d/namespaceinventory.html#af3a4ddfc14bf0cce9255798e5adfba8c',1,'inventory']]],
   ['clearmap_3',['ClearMap',['../d3/d2c/namespacegame.html#a45922950e1a53294c947464f95e2a26b',1,'game']]],
