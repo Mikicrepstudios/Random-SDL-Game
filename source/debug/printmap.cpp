92f3bd01223a80e18cc56c5b7955ab1e5a9795ae
Mikicrep <mikicrepoffical@gmail.com>
2025-01-20 00:05:29 +0100

    Add title printmap.cpp

--- source/debug/printmap.cpp
+++ source/debug/printmap.cpp
@@ -4,6 +4,7 @@
 
 namespace debug {
     void PrintMap(game::Map map) {
+        std::cout << "DEBUG - MAP" << std::endl;
         std::cout << "Width: " << map.width << std::endl
                   << "Height: " << map.height << std::endl;
     }
