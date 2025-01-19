56321bab0018e9a5186e3e25cd94c70cf067736c
Mikicrep <mikicrepoffical@gmail.com>
2025-01-19 23:57:40 +0100

    Add text printsettings.cpp

--- source/debug/printsettings.cpp
+++ source/debug/printsettings.cpp
@@ -4,21 +4,21 @@
 
 namespace debug {
     void PrintSettings(game::Settings settings) {
-        std::cout << settings.curPreset << std::endl
-                  << settings.blockColor << std::endl
-                  << settings.blockTextureId << std::endl
-                  << settings.bgColor << std::endl
-                  << settings.playerColor << std::endl
-                  << settings.canPlayerPlace << std::endl
-                  << settings.placeSolidBlocks << std::endl
-                  << settings.inventory << std::endl
-                  << settings.colorPickerId << std::endl
-                  << settings.colorPicker << std::endl
-                  << settings.texturePicker << std::endl
-                  << settings.dialogueId << std::endl
-                  << settings.dialogue << std::endl
-                  << settings.cheatsId << std::endl
-                  << settings.cheats << std::endl
-                  << settings.gameInfo << std::endl;
+        std::cout << "Cur preset: " << settings.curPreset << std::endl
+                  << "Block color: " << settings.blockColor << std::endl
+                  << "Block texture ID: " << settings.blockTextureId << std::endl
+                  << "BG Color: " << settings.bgColor << std::endl
+                  << "Player Color: " << settings.playerColor << std::endl
+                  << "Can player place: " << settings.canPlayerPlace << std::endl
+                  << "Place solid blocks: " << settings.placeSolidBlocks << std::endl
+                  << "Inventory: " << settings.inventory << std::endl
+                  << "Color picker ID: " << settings.colorPickerId << std::endl
+                  << "Color picker: " << settings.colorPicker << std::endl
+                  << "Texture picker: " << settings.texturePicker << std::endl
+                  << "Dialogue ID: " << settings.dialogueId << std::endl
+                  << "Dialogue: " << settings.dialogue << std::endl
+                  << "Cheats ID: " << settings.cheatsId << std::endl
+                  << "Cheats: " << settings.cheats << std::endl
+                  << "Game info: " << settings.gameInfo << std::endl;
     }
 }
