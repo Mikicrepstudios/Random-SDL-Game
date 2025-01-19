9373918917a8558f029cf35b648591ea8f415c22
Mikicrep <mikicrepoffical@gmail.com>
2025-01-19 23:09:59 +0100

    debug.h

---
+++ source/headers/debug.h
@@ -0,0 +1,9 @@
+#include "settings.h"
+
+namespace debug {
+    void printGame(game::Game game);
+    void printSettings(game::Settings settings);
+    void printMap(game::Map map);
+    void printPlayer(game::Player player);
+    void printCamera(game::Camera camera);
+}
