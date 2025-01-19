27eafacd36b1889a83795c576dfe02e282a0d689
Mikicrep <mikicrepoffical@gmail.com>
2025-01-20 00:05:42 +0100

    Add title printcamera.cpp

--- source/debug/printcamera.cpp
+++ source/debug/printcamera.cpp
@@ -4,6 +4,7 @@
 
 namespace debug {
     void PrintCamera(game::Camera cam) {
+        std::cout << "DEBUG - CAMERA" << std::endl;
         std::cout << "Scale: " << cam.scale << std::endl
                   << "OffsetX: " << cam.offSetX << std::endl
                   << "OffsetY: " << cam.offSetY << std::endl
