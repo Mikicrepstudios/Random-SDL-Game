44f367764b0b7d15fd88833c05954545190d7243
Mikicrep <mikicrepoffical@gmail.com>
2025-01-19 23:55:12 +0100

    Add text printgame.cpp

--- source/debug/printgame.cpp
+++ source/debug/printgame.cpp
@@ -4,9 +4,9 @@
 
 namespace debug {
     void PrintGame(game::Game game) {
-        std::cout << game.terminalmode << std::endl
-                  << game.cliInput << std::endl
-                  << game.curHoverX << std::endl
-                  << game.curHoverY << std::endl;
+        std::cout << "Terminal Mode: " << game.terminalmode << std::endl
+                  << "CLI input: " << game.cliInput << std::endl
+                  << "Cur hover X: " << game.curHoverX << std::endl
+                  << "Cur hover Y: " << game.curHoverY << std::endl;
     }
 }
