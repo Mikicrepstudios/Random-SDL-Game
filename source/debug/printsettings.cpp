3c12d74a16f4ba39a7fa64e7c07b6e10f43f518f
Mikicrep <mikicrepoffical@gmail.com>
2025-01-19 23:30:00 +0100

    debug::printsettings()

---
+++ source/debug/printsettings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "settings.h"
+
+namespace debug {
+    void printSettings(game::Settings settings) {
+        std::cout << settings.curPreset << std::endl
+                  << settings.blockColor << std::endl
+                  << settings.blockTextureId << std::endl
+                  << settings.bgColor << std::endl
+                  << settings.playerColor << std::endl
+                  << settings.canPlayerPlace << std::endl
+                  << settings.placeSolidBlocks << std::endl
+                  << settings.inventory << std::endl
+                  << settings.colorPickerId << std::endl
+                  << settings.colorPicker << std::endl
+                  << settings.texturePicker << std::endl
+                  << settings.dialogueId << std::endl
+                  << settings.dialogue << std::endl
+                  << settings.cheatsId << std::endl
+                  << settings.cheats << std::endl
+                  << settings.gameInfo << std::endl;
+    }
+}
