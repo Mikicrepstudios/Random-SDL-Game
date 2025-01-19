7e36734b6cdba5f6fc682370841f410a58dad619
Mikicrep <mikicrepoffical@gmail.com>
2025-01-20 00:16:29 +0100

    Document printgame.cpp

--- source/debug/printgame.cpp
+++ source/debug/printgame.cpp
@@ -4,6 +4,10 @@
 
 namespace debug {
     void PrintGame(game::Game game) {
+        /**
+         * @brief Prints all values from game::Game struct
+         * @param game Core settings
+         */
         std::cout << "DEBUG - GAME" << std::endl;
         std::cout << "Terminal Mode: " << game.terminalmode << std::endl
                   << "CLI input: " << game.cliInput << std::endl
