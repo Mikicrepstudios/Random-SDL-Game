f8081e3c15d437789ad5bb9f85fdd75c411067e8
Mikicrep <mikicrepoffical@gmail.com>
2025-01-20 00:05:06 +0100

    Add title printgame.cpp

--- source/debug/printgame.cpp
+++ source/debug/printgame.cpp
@@ -4,6 +4,7 @@
 
 namespace debug {
     void PrintGame(game::Game game) {
+        std::cout << "DEBUG - GAME" << std::endl;
         std::cout << "Terminal Mode: " << game.terminalmode << std::endl
                   << "CLI input: " << game.cliInput << std::endl
                   << "Cur hover X: " << game.curHoverX << std::endl
