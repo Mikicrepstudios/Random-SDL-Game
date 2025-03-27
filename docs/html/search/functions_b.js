72f2b91b0a61f0d09bcf32f25a9077f2f5b75111
Mikicrep <mikicrepoffical@gmail.com>
2025-03-27 01:42:35 +0100

    Update docs

--- docs/html/search/functions_b.js
+++ docs/html/search/functions_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['terminalmode_0',['TerminalMode',['../d5/df2/namespacecommands.html#a309f0bdd08964b575d9b20dc70e54c6a',1,'commands']]]
+  ['terminalmode_0',['TerminalMode',['../d5/df2/namespacecommands.html#a309f0bdd08964b575d9b20dc70e54c6a',1,'commands']]],
+  ['tpcam_1',['TpCam',['../d5/df2/namespacecommands.html#a05bb033f8f59bf71d73e837bbc68d18a',1,'commands']]]
 ];
--- docs/html/search/variables_d.js
+++ docs/html/search/variables_d.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['terminalmode_0',['terminalmode',['../d4/d62/structgame_1_1Game.html#a3909c10238221b8f5a0643671c98113a',1,'game::Game']]],
   ['texture_1',['texture',['../df/d68/structBlock.html#a2321625fc1273c7b18ea5b3422295962',1,'Block::texture'],['../d3/d7e/structtextures_1_1BlockTextures.html#a68ef8aab7578cd4632bf1c456b17b2dd',1,'textures::BlockTextures::texture']]],
-  ['textureidrect_2',['textureIdRect',['../d9/d08/structinventory_1_1DecalRects.html#a3585f05de34a5758658710bfdac241af',1,'inventory::DecalRects']]],
-  ['textureidrectb_3',['textureIdRectb',['../d9/d08/structinventory_1_1DecalRects.html#a064c306fa4f6264f5d97fea741d8a98b',1,'inventory::DecalRects']]],
-  ['textureidtextrect_4',['textureIdTextRect',['../d9/d08/structinventory_1_1DecalRects.html#a48f7fd9cb014583f55aa7b7851a0342a',1,'inventory::DecalRects']]],
-  ['texturepicker_5',['texturePicker',['../db/d51/structgame_1_1Settings.html#a133f70bdb53c20c9a05ba098b84fb181',1,'game::Settings']]],
-  ['titlerect_6',['titleRect',['../d1/d44/structdialogues_1_1rects.html#affe013bc92fdda84b29f0af0c4bd9156',1,'dialogues::rects']]],
-  ['type_7',['type',['../df/d68/structBlock.html#a64ecd5e3cc8ce5a407829bcfc1d11789',1,'Block']]]
+  ['textureid_2',['textureId',['../d4/d90/structgame_1_1Preset.html#ab9e82ea9bb398b030267e6ca22dc4ff1',1,'game::Preset']]],
+  ['textureidrect_3',['textureIdRect',['../d9/d08/structinventory_1_1DecalRects.html#a3585f05de34a5758658710bfdac241af',1,'inventory::DecalRects']]],
+  ['textureidrectb_4',['textureIdRectb',['../d9/d08/structinventory_1_1DecalRects.html#a064c306fa4f6264f5d97fea741d8a98b',1,'inventory::DecalRects']]],
+  ['textureidtextrect_5',['textureIdTextRect',['../d9/d08/structinventory_1_1DecalRects.html#a48f7fd9cb014583f55aa7b7851a0342a',1,'inventory::DecalRects']]],
+  ['texturepicker_6',['texturePicker',['../db/d51/structgame_1_1Settings.html#a133f70bdb53c20c9a05ba098b84fb181',1,'game::Settings']]],
+  ['titlerect_7',['titleRect',['../d1/d44/structdialogues_1_1rects.html#affe013bc92fdda84b29f0af0c4bd9156',1,'dialogues::rects']]],
+  ['type_8',['type',['../df/d68/structBlock.html#a64ecd5e3cc8ce5a407829bcfc1d11789',1,'Block']]]
 ];
