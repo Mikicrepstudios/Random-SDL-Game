6b1082a1dd56482ba590dae831f517a0fbe2400a
Mikicrep <mikicrepoffical@gmail.com>
2025-03-27 02:39:46 +0100

    Update docs

--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -10,12 +10,14 @@ var searchData=
   ['setbgcolor_2ecpp_7',['setbgcolor.cpp',['../da/dff/setbgcolor_8cpp.html',1,'']]],
   ['setblockcolor_8',['SetBlockColor',['../d5/df2/namespacecommands.html#ad9551f11c93a4280efd328deb319fe83',1,'commands']]],
   ['setblockcolor_2ecpp_9',['setblockcolor.cpp',['../d9/d30/setblockcolor_8cpp.html',1,'']]],
-  ['setplayercolor_10',['SetPlayerColor',['../d5/df2/namespacecommands.html#a3f5c20970d2865172424c5a32784b4b9',1,'commands']]],
-  ['setplayercolor_2ecpp_11',['setplayercolor.cpp',['../d8/d68/setplayercolor_8cpp.html',1,'']]],
-  ['settings_12',['Settings',['../db/d51/structgame_1_1Settings.html',1,'game']]],
-  ['settings_2eh_13',['settings.h',['../de/d60/settings_8h.html',1,'']]],
-  ['solidrect_14',['solidRect',['../d3/d20/structinventory_1_1OtherRects.html#a5b5e6236119664928c559a7c7aae370f',1,'inventory::OtherRects']]],
-  ['solidrectb_15',['solidRectb',['../d3/d20/structinventory_1_1OtherRects.html#ae392a8fae312e19050e3f01d26d4eca0',1,'inventory::OtherRects']]],
-  ['solidtextrect_16',['solidTextRect',['../d3/d20/structinventory_1_1OtherRects.html#a71b4b2a10432295c44f012167bbbe4a9',1,'inventory::OtherRects']]],
-  ['speed_17',['speed',['../d0/ddf/structgame_1_1Player.html#a33cbb53abd8f1f236cf1113f625b2ca1',1,'game::Player']]]
+  ['setblocktexture_10',['SetBlockTexture',['../d5/df2/namespacecommands.html#acc76ee73cf2ac23892f1d6f66f971e79',1,'commands']]],
+  ['setblocktexture_2ecpp_11',['setblocktexture.cpp',['../d0/dc7/setblocktexture_8cpp.html',1,'']]],
+  ['setplayercolor_12',['SetPlayerColor',['../d5/df2/namespacecommands.html#a3f5c20970d2865172424c5a32784b4b9',1,'commands']]],
+  ['setplayercolor_2ecpp_13',['setplayercolor.cpp',['../d8/d68/setplayercolor_8cpp.html',1,'']]],
+  ['settings_14',['Settings',['../db/d51/structgame_1_1Settings.html',1,'game']]],
+  ['settings_2eh_15',['settings.h',['../de/d60/settings_8h.html',1,'']]],
+  ['solidrect_16',['solidRect',['../d3/d20/structinventory_1_1OtherRects.html#a5b5e6236119664928c559a7c7aae370f',1,'inventory::OtherRects']]],
+  ['solidrectb_17',['solidRectb',['../d3/d20/structinventory_1_1OtherRects.html#ae392a8fae312e19050e3f01d26d4eca0',1,'inventory::OtherRects']]],
+  ['solidtextrect_18',['solidTextRect',['../d3/d20/structinventory_1_1OtherRects.html#a71b4b2a10432295c44f012167bbbe4a9',1,'inventory::OtherRects']]],
+  ['speed_19',['speed',['../d0/ddf/structgame_1_1Player.html#a33cbb53abd8f1f236cf1113f625b2ca1',1,'game::Player']]]
 ];
