37034f10d935cf3f98d407b568994e0af4121438
Mikicrep <mikicrepoffical@gmail.com>
2025-07-29 14:14:30 +0200

    Whole new save system!!!

--- source/headers/files.h
+++ source/headers/files.h
@@ -8,8 +8,8 @@
  * @brief Namespace that contains all functions that are related to managing files on disk
  */
 namespace files {
-	void SaveMap(game::Map map);
-	void SaveSettings(game::Settings settings, game::Player player, game::Camera cam);
-	void LoadMap(game::Map &map);
-	void LoadSettings(game::Settings &settings, game::Player &player, game::Camera &cam, game::Preset preset[10]);
+	void LoadGame_v1(game::Map& map, game::Settings& settings, game::Player& player, game::Camera& cam, std::ifstream& file);
+
+	void SaveGame(const game::Map& map, const game::Settings& settings, const game::Player& player, const game::Camera& cam, const std::string& slotName);
+	void LoadGame(game::Map& map, game::Settings& settings, game::Player& player, game::Camera& cam, const std::string& saveName);
 }	
