a77a5462bbf82557e192184fcf3d40ac1de3139a
Mikicrep <mikicrepoffical@gmail.com>
2025-08-04 11:24:15 +0200

    Legacy map load function

--- source/headers/files.h
+++ source/headers/files.h
@@ -9,6 +9,7 @@
  */
 namespace files {
 	void LoadGame_v1(game::Map& map, game::Settings& settings, game::Player& player, game::Camera& cam, std::ifstream& file);
+	void LoadLegacyMap(game::Map &map); // Unused, but kept for legacy support
 
 	void SaveGame(const game::Map& map, const game::Settings& settings, const game::Player& player, const game::Camera& cam, const std::string& slotName);
 	void LoadGame(game::Map& map, game::Settings& settings, game::Player& player, game::Camera& cam, const std::string& saveName);
